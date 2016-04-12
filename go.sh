90a3c31508130792201195770d45ea0b0ac40ace
mamengmeng <3139728023@qq.com>
2016-04-12 17:15:45 +0800

    新增上传脚本

---
+++ go.sh
@@ -0,0 +1,7 @@
+git config --global user.email "3139728023@qq.com"
+git config --global user.name "mamengmeng"
+git config --global push.defaul simple
+git add .
+git commit -m $1
+git push
+start http://mamengmeng.github.io
